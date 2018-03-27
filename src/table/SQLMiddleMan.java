7343dae00a7e411511c67101de40563056d9af2e
Kurt Gregorek <kxg1491@g.rit.edu>
2018-03-26 23:22:17 -0400

    Add sql stuff

---
+++ src/table/SQLMiddleMan.java
@@ -0,0 +1,8 @@
+package table;
+
+public class SQLMiddleMan
+{
+	public void executeQuery(String cmd){
+		//execute sql called
+	}
+}
---
+++ src/table/Schema.java
@@ -0,0 +1,22 @@
+package table;
+
+import java.util.ArrayList;
+
+public class Schema
+{
+	private ArrayList<Property> properties;
+	private ArrayList<Customer> customers;
+	private ArrayList<Employee> employees;
+	private ArrayList<Sale> sales;
+	private ArrayList<Appointment> appointments;
+	
+	public Schema(){
+		//create schema here
+	}
+	
+	
+	private void load(){
+		//Load from sql here
+	}
+	
+}
